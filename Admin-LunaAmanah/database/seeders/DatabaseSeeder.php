8bc5917e98c43f3815fc6f7eba6d6f487186994e
Moh. Abib Safaqdillah <62880645+abif47@users.noreply.github.com>
2023-01-01 18:57:25 +0800

    Test CRUD Post Sederhana

---
+++ Admin-LunaAmanah/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //\App\Models\User::factory()->create([
+        //    'name' => 'Test User',
+        //    'email' => 'test@example.com',
+        //]);
+        User::create([
+            'name' => 'Abib Safaqdillah',
+            'username' => 'abib',
+            'email' => 'abibsafaqdillah@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+        // User::create([
+        //     'name' => 'Mohammad Abib',
+        //     'email' => 'mohammadabibtest@gmail.com',
+        //     'password' => bcrypt('54321')
+        // ]);
+        \App\Models\User::factory(3)->create();
+        Category::create([
+            'name' => 'Haji',
+            'slug' => 'haji',
+        ]);
+        Category::create([
+            'name' => 'Umrah',
+            'slug' => 'umrah',
+        ]);
+        Category::create([
+            'name' => 'Tour',
+            'slug' => 'tour',
+        ]);
+        \App\Models\Post::factory(20)->create();
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum pertama',
+        //     'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, vero exercitationem. In, tenetur vero!</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, cupiditate aliquam esse quasi ex expedita eveniet dignissimos debitis dolorum quibusdam aspernatur sint itaque non? Aliquid maiores dignissimos, molestiae consectetur, quis dolore, quas ex ab atque praesentium minus voluptatum molestias. Voluptatibus, rem dicta. Aliquid, autem laudantium! Amet praesentium quod facere animi.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores incidunt amet placeat alias eveniet cumque qui beatae excepturi? Blanditiis consequuntur vero illo ratione? Nostrum et labore ut, facere magni animi! Accusantium, ullam reprehenderit tempore, aliquid aperiam eum vitae repellat sint numquam laborum totam ipsa dolores. Non quasi itaque adipisci ullam.</p>'
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum kedua',
+        //     'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, vero exercitationem. In, tenetur vero!</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, cupiditate aliquam esse quasi ex expedita eveniet dignissimos debitis dolorum quibusdam aspernatur sint itaque non? Aliquid maiores dignissimos, molestiae consectetur, quis dolore, quas ex ab atque praesentium minus voluptatum molestias. Voluptatibus, rem dicta. Aliquid, autem laudantium! Amet praesentium quod facere animi.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores incidunt amet placeat alias eveniet cumque qui beatae excepturi? Blanditiis consequuntur vero illo ratione? Nostrum et labore ut, facere magni animi! Accusantium, ullam reprehenderit tempore, aliquid aperiam eum vitae repellat sint numquam laborum totam ipsa dolores. Non quasi itaque adipisci ullam.</p>'
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'category_id' => 2,
+        //     'user_id' => 1,
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum ketiga',
+        //     'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, vero exercitationem. In, tenetur vero!</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, cupiditate aliquam esse quasi ex expedita eveniet dignissimos debitis dolorum quibusdam aspernatur sint itaque non? Aliquid maiores dignissimos, molestiae consectetur, quis dolore, quas ex ab atque praesentium minus voluptatum molestias. Voluptatibus, rem dicta. Aliquid, autem laudantium! Amet praesentium quod facere animi.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores incidunt amet placeat alias eveniet cumque qui beatae excepturi? Blanditiis consequuntur vero illo ratione? Nostrum et labore ut, facere magni animi! Accusantium, ullam reprehenderit tempore, aliquid aperiam eum vitae repellat sint numquam laborum totam ipsa dolores. Non quasi itaque adipisci ullam.</p>'
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'category_id' => 2,
+        //     'user_id' => 2,
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Lorem ipsum keempat',
+        //     'body' => '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, vero exercitationem. In, tenetur vero!</p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, cupiditate aliquam esse quasi ex expedita eveniet dignissimos debitis dolorum quibusdam aspernatur sint itaque non? Aliquid maiores dignissimos, molestiae consectetur, quis dolore, quas ex ab atque praesentium minus voluptatum molestias. Voluptatibus, rem dicta. Aliquid, autem laudantium! Amet praesentium quod facere animi.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores incidunt amet placeat alias eveniet cumque qui beatae excepturi? Blanditiis consequuntur vero illo ratione? Nostrum et labore ut, facere magni animi! Accusantium, ullam reprehenderit tempore, aliquid aperiam eum vitae repellat sint numquam laborum totam ipsa dolores. Non quasi itaque adipisci ullam.</p>'
+        // ]);
+    }
+}
---
+++ Admin-LunaAmanah/resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+@section('container')
+    <h1>Halaman About</h1>
+    <h3>{{ $nama }}</h3>
+    <h3>{{ $email }}</h3>
+    <h3>{{ $Universitas }}</h3>
+@endsection
